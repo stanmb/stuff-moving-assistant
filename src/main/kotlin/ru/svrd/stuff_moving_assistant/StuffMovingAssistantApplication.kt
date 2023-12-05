000fa36af1a224917f81770fa543c89d20122984
Dmitrii Sviridov <d.sviridov@mcplat.ru>
2023-12-05 18:57:02 +0300

    initial

---
+++ src/main/kotlin/ru/svrd/stuff_moving_assistant/StuffMovingAssistantApplication.kt
@@ -0,0 +1,11 @@
+package ru.svrd.stuff_moving_assistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StuffMovingAssistantApplication
+
+fun main(args: Array<String>) {
+	runApplication<StuffMovingAssistantApplication>(*args)
+}
